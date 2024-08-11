12f6d5b0a6350100c1255eebdbe1f79f624beb8d
BehruzbekX <bahaboy761@gmail.com>
2024-08-11 16:02:39 +0500

    Update fenwick.cpp

--- fenwick.cpp
+++ fenwick.cpp
@@ -5,21 +5,21 @@
 template<typename T> struct Fenwick {
     vector<T> bit;
     int n;
-    const int INF = 1e9;
+    const int INF = 1e9 + 1;
     int op(int x,int y){
         int z = (x + y);
         return z;
     }
     Fenwick(int n) {
         this->n = n;
-        bit.assign(n + 1, 0);//may change, N for min
+        bit.assign(n + 1, 0);//may change, INF for min,-INF for max
     }
     Fenwick(vector<T> const &a) : Fenwick(a.size()) {
         for (size_t i = 0; i < a.size(); i++)
             add(i, a[i]);
     }
     T get(int r) {
-        int res = 0;//may change, N for min
+        int res = 0;//may change, INF for min,-INF for max
         for (; r >= 0; r = (r & (r + 1)) - 1)
             res = op(res,bit[r]);
         return res;
