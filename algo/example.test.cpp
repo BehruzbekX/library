87f91ce207b3dabd697c064e647b6f901770e9ce
BehruzbekX <bahaboy761@gmail.com>
2024-08-21 14:35:22 +0500

    a

--- algo/example.test.cpp
+++ algo/example.test.cpp
@@ -1,7 +1,7 @@
 
 #include "my_template.hpp"
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/4/ALDS1_4_B"
-
+#include <my_template.hpp>
 using namespace std;
 
 signed main() {
