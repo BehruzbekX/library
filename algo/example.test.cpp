7a6fd0a01dc4087e09d9ed2c75ea98930e101193
BehruzbekX <bahaboy761@gmail.com>
2024-08-21 14:33:21 +0500

    a

--- algo/example.test.cpp
+++ algo/example.test.cpp
@@ -1,26 +1,18 @@
+
 #include "my_template.hpp"
-
-
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/4/ALDS1_4_B"
-#include <algorithm>
-#include <iostream>
-#include <vector>
-#define REP(i, n) for (int i = 0; (i) < (int)(n); ++ (i))
-#define ALL(x) std::begin(x), std::end(x)
+
 using namespace std;
 
-int main() {
-    int n; cin >> n;
-    VI<int> s(n);
-    REP (i, n) {
-        cin >> s[i];
-    }
-    int q; cin >> q;
+signed main() {
+    INT(n);
+    VEC(int,a,n);
+    INT(q);
     int cnt = 0;
     while (q --) {
-        int t_i; cin >> t_i;
+        INT(t_i);
         cnt += binary_search(ALL(s), t_i);
     }
-    cout << cnt << endl;
+    W(cnt);
     return 0;
 }
