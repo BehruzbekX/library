2155b42e336f6a65104e431d5d3d413abd73c64e
BehruzbekX <bahaboy761@gmail.com>
2024-08-22 22:02:53 +0500

    a

---
+++ random/shuffle.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "random/base.hpp"
+
+template<class T>
+void shuffle(vi<T> &A){
+    FOR(i,SZ(A)){
+        int j = RNG(0,i + 1);
+        if(i != j) swap(A[i],A[j]);
+    }
+}
