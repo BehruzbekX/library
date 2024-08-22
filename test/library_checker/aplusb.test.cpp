706d3b62f5e90de0860b4bd7537a6ab410f07c79
BehruzbekX <bahaboy761@gmail.com>
2024-08-22 21:41:52 +0500

    a

---
+++ test/library_checker/aplusb.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+
+#include "template.hpp"
+#include "other/fastio.hpp"
+#include "random/base.hpp"
+
+void solve() {
+     LL(A,B);
+     W(A + B);
+}
+
+signed main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    // TEST
+
+    solve();
+}
